d3b39b2723566c31f17e50dce6475e90cfcb11e7
duyong <33488733+duyongtju@users.noreply.github.com>
2019-08-13 20:46:50 +0800

    Update EventLoopThreadPool.h

--- net/EventLoopThreadPool.h
+++ net/EventLoopThreadPool.h
@@ -1,13 +1,3 @@
-// Copyright 2010, Shuo Chen.  All rights reserved.
-// http://code.google.com/p/muduo/
-//
-// Use of this source code is governed by a BSD-style license
-// that can be found in the License file.
-
-// Author: Shuo Chen (chenshuo at chenshuo dot com)
-//
-// This is an internal header file, you should not include this.
-
 #ifndef MUDUO_NET_EVENTLOOPTHREADPOOL_H
 #define MUDUO_NET_EVENTLOOPTHREADPOOL_H
 
