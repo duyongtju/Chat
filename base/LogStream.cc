09506559a67dddb687a697550b018e389141a683
duyong <33488733+duyongtju@users.noreply.github.com>
2019-08-13 21:13:30 +0800

    Update LogStream.cc

--- base/LogStream.cc
+++ base/LogStream.cc
@@ -1,8 +1,3 @@
-// Use of this source code is governed by a BSD-style license
-// that can be found in the License file.
-//
-// Author: Shuo Chen (chenshuo at chenshuo dot com)
-
 #include "muduo/base/LogStream.h"
 
 #include <algorithm>
